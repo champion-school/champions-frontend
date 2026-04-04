d55bc662eb08783e7e18e71cbcacd605f2c69208
23suca52 <23suca52@tcarts.in>
2026-04-04 19:41:34 +0530

    nav bar correction

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import './Header.css'
 import NavLogo from '../pages/img/champion.png'
 
